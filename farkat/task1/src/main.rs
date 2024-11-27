319d94174ed05629e77d58bd057ef8322094be95
supiz7z <burubulka12@gmail.com>
2024-11-28 00:48:00 +0300

    edit: task 1, task 2

--- farkat/task1/src/main.rs
+++ farkat/task1/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("task1");
+
+    let num = 8;
+    let mut result_num = 0;
+    let mut sum_string:String = Default::default();
+
+    for i in 1..num+1 {
+        result_num += i;
+        let substring = if i != num {format!("{} + ", i)} else {format!("{}", i)};
+        sum_string.push_str(&substring);
+    }
+
+    let output = format!("{} -> {} ({})", num, result_num, sum_string);
+    println!("task 1\ninput: {}\noutput: {}", num, output);
 }
--- farkat/task2/src/main.rs
+++ farkat/task2/src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-
     let a = 100_i32;
-    println!("task2\ninput: {}\nresult: {}", a,make_negative(a));
+    println!("task 2\ninput: {}\noutput: {}", a, make_negative(a));
 }
 
-fn make_negative(i: i32) -> i32 {
-    if i <= 0 {return i};
-    -i
-}
+fn make_negative(i: i32) -> i32 { if i <= 0 { i } else { -i } }
