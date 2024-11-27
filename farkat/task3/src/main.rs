b0f4873d5977b017072b544a15a2d3d3d87e4046
supiz7z <burubulka12@gmail.com>
2024-11-28 01:17:39 +0300

    add: task 3

--- farkat/task3/src/main.rs
+++ farkat/task3/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("task3");
+    let mut vowels:HashMap<String, i32> = HashMap::new();
+    vowels.insert("a".parse().unwrap(), 1);
+    vowels.insert("e".parse().unwrap(), 1);
+    vowels.insert("i".parse().unwrap(), 1);
+    vowels.insert("o".parse().unwrap(), 1);
+    vowels.insert("u".parse().unwrap(), 1);
+
+    let mut count = 0;
+    let input_string = String::from("hello world yyy aiu");
+    for char in input_string.chars() {
+        if vowels.contains_key(&char.to_string()) {
+            count += 1;
+        }
+    }
+    println!("task 3\ninput: {}\noutput: vowels count = {}", input_string, count);
 }
